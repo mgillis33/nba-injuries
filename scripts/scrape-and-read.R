f7b3190483cdbfa848189c1716b0f5d694899b17
mgillis33 <gillism@emmanuel.edu>
2023-04-14 18:14:16 -0400

    Finished first draft for now!

--- scripts/scrape-and-read.R
+++ scripts/scrape-and-read.R
@@ -40,8 +40,17 @@ df <- df %>%
     notes = X5
   )
 
-df <- subset(df, df$acquired == "")
-df <- subset(df, !(df$name == ""))
+df <- subset(df, df$acquired == "" &
+               df$name != "" &
+               !(grepl("head coach", df$notes)) &
+               !(grepl("illness", df$notes)) &
+               !(grepl("flu", df$notes)) &
+               !(grepl("virus", df$notes)) &
+               !(grepl("health", df$notes)) &
+               !(grepl("personal", df$notes)) &
+               !(grepl("personal", df$notes)) &
+               !(grepl("child", df$notes))
+             )
 
 df <- df %>% 
   select(date, team, name, notes) %>% 
@@ -51,24 +60,26 @@ df <- df %>%
 injuries <- df %>% 
   count(name, name = "injuries")
 
-players <- read.csv("data/all_seasons.csv")
+df2 <- read.csv("data/all_seasons.csv")
 
-players <- players %>% 
+df2 <- df2 %>% 
   select(!c(X, team_abbreviation, age)) %>% 
   rename(name = player_name) %>% 
   mutate(player_weight = player_weight * 2.20462) %>% 
   mutate(player_height = player_height * 0.393701) %>% 
+  mutate(bmi = (player_weight/(player_height^2))*703) %>% 
   mutate(season = gsub("\\.-*", "", season)) %>% 
   mutate(name = gsub("\\ /.*", "", name))
 
-players <- players %>% 
+players <- df2 %>% 
   group_by(name) %>% 
-  summarize(across(c(player_height, player_weight, gp, pts, reb, ast, net_rating, oreb_pct, dreb_pct, usg_pct, ts_pct, ast_pct), mean))
+  summarize(across(c(player_height, player_weight, gp, pts, reb, ast, net_rating, oreb_pct, dreb_pct, usg_pct, ts_pct, ast_pct, bmi), mean)) %>% 
+  mutate(across(where(is.numeric), round, 2))
 
 full_table <- inner_join(injuries, players, by = "name")
 
 full_table %>% 
-  ggplot(aes(x = player_weight)) +
+  ggplot(aes(x = bmi)) +
   geom_histogram() +
   theme_bw()
 
@@ -78,13 +89,71 @@ full_table %>%
   theme_bw()
 
 full_table %>% 
-  ggplot(aes(x = player_weight, y = injuries)) +
+  ggplot(aes(x = bmi, y = injuries)) +
   geom_point() +
   geom_smooth(method = "lm") +
   theme_bw()
 
-injuries_weight <- linear_reg() %>% 
+injuries_bmi <- linear_reg() %>% 
   set_engine("lm") %>% 
-  fit(player_weight ~ injuries, data = full_table)
+  fit(bmi ~ injuries, data = full_table)
 
-glance(injuries_weight)
+parsnip::tidy(injuries_bmi)
+
+glance(injuries_bmi)
+
+lm_stats <- function(variable_of_interest){
+  
+  response_variable <- full_table %>% 
+    pull(injuries)
+  
+  explanatory_variable <- full_table %>% 
+    pull(variable_of_interest)
+  
+  new_df <- data.frame(response_variable, explanatory_variable)
+  
+  new_fit <- linear_reg() %>% 
+    set_engine("lm") %>% 
+    fit(response_variable ~ explanatory_variable, data = new_df )
+  
+  slope <- tidy(new_fit) %>% 
+    filter(term == "explanatory_variable") %>%
+    pull(estimate)
+  
+  r_squared <- glance(new_fit) %>% 
+    pull(r.squared)
+  
+  p_value <- glance(new_fit) %>% 
+    pull(p.value)
+  
+  return(
+    list(
+      slope = slope, 
+      r_squared = r_squared, 
+      p_value = p_value
+    )
+  )
+  
+}
+
+variable_names <- full_table %>% 
+  select_if(is.numeric) %>%
+  select(!injuries) %>%
+  names()
+
+injury_models <- data.frame(variable_names,slope = NA, r_squared = NA, p_value = NA)
+
+row_index <- 1
+
+for(row_index in 1:nrow(injury_models)){
+  
+  stats_results <- lm_stats(variable_of_interest = injury_models[row_index, "variable_names"])
+  
+  injury_models[row_index, "slope"] <- stats_results$slope
+  injury_models[row_index,"r_squared"] <- stats_results$r_squared
+  injury_models[row_index,"p_value"] <- stats_results$p_value
+  
+}
+
+injury_models %>% 
+  arrange(desc(r_squared))
